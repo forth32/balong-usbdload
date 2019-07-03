ed90dcf7d77a7fbdf30a6ec76459c6d17b3bffa8
ValdikSS <iam@valdikss.org.ru>
2019-07-03 14:34:51 +0300

    pv7r22_3 loader patch signature for B525s and B529

--- patcher.h
+++ patcher.h
@@ -19,6 +19,7 @@ uint32_t patch(struct defpatch fp, uint8_t* buf, uint32_t fsize, uint32_t ptype)
 
 uint32_t pv7r22 (uint8_t* buf, uint32_t fsize);
 uint32_t pv7r22_2 (uint8_t* buf, uint32_t fsize);
+uint32_t pv7r22_3 (uint8_t* buf, uint32_t fsize);
 uint32_t pv7r2 (uint8_t* buf, uint32_t fsize);
 uint32_t pv7r11 (uint8_t* buf, uint32_t fsize);
 uint32_t pv7r1 (uint8_t* buf, uint32_t fsize);
